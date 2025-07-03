403d8435896cabb984cb7a181722d51eb3953b5b
Artem Makeev <Kochebnic@gmail.com>
2025-07-03 17:50:30 +0300

    /sendMgs сделан

--- src/Orchestrator/Telegram/Directives/SendMgs.ts
+++ src/Orchestrator/Telegram/Directives/SendMgs.ts
@@ -8,11 +8,11 @@ import { parseCommand } from "../Utils/ScriptParse";
 class SendMgs implements OrchestratorTelegramInterface.IClass {
 	public async invoke(modules: ProjectInterface.TDIService, data: TelegramInterface.IUpdate) {
 		const text = parseCommand(data.message?.text || "").text;
-		const [id, word] = text.split(" ");
+		const [id, ...word] = text.split(" ");
 
 		const isUserExist = modules("Auth").invoke.isAuthUser(+id, OrchestratorTelegramInterface.EDirective.SAY);
 		if (!isUserExist) return;
 
-		await modules("Telegram").invoke.sendMessage(word, +id);
+		await modules("Telegram").invoke.sendMessage(word.join(" "), +id);
 	}
 }
