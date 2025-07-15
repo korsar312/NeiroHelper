ec9eca996a91f287095800fee7e1308c3c7190f1
Artem Makeev <Kochebnic@gmail.com>
2025-07-15 07:11:54 +0300

    /transfer сделан

--- src/Config/Const.ts
+++ src/Config/Const.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const Const = {
-	payToDay: process.env.IS_DEV ? 100 : 3,
+	payToDay: process.env.IS_DEV ? 3 : 100,
 	historyQty: 20,
 	awaitPay: 500,
 	superPartPercent: 25,
