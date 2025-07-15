facaa08c29edaea446de20deac65e006c587362f
Artem Makeev <Kochebnic@gmail.com>
2025-07-16 01:38:49 +0300

    /transfer сделан

--- src/Config/Secret.ts
+++ src/Config/Secret.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const Secret = {
-	tokenTg: (process.env.IS_DEV ? process.env.TOKEN_TG_TEST : process.env.TOKEN_TG) || "",
-	openAiToken: process.env.OPENAI_TOKEN || "",
-	addressWalletPidor: process.env.ADDRESS_WALLET_PIDOR || "",
-	addressWalletSuper: process.env.ADDRESS_WALLET_SUPER || "",
-	addressWalletWork: process.env.ADDRESS_WALLET_WORK || "",
-	tokenWalletWork: process.env.TOKEN_WALLET_WORK || "",
-	usdtContract: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+	tokenTg: ((process.env.IS_DEV ? process.env.TOKEN_TG_TEST : process.env.TOKEN_TG) || "").trim(),
+	openAiToken: (process.env.OPENAI_TOKEN || "").trim(),
+	addressWalletPidor: (process.env.ADDRESS_WALLET_PIDOR || "").trim(),
+	addressWalletSuper: (process.env.ADDRESS_WALLET_SUPER || "").trim(),
+	addressWalletWork: (process.env.ADDRESS_WALLET_WORK || "").trim(),
+	tokenWalletWork: (process.env.TOKEN_WALLET_WORK || "").trim(),
+	usdtContract: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t".trim(),
 };
