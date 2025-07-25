11e8d304dbcc83d0e559073ce49b175a8e1a1ee6
Artem Makeev <Kochebnic@gmail.com>
2025-07-25 03:13:55 +0300

    масштабирование нейросети

--- src/Services/ServiceTelegram/Imp/Telegram.imp.ts
+++ src/Services/ServiceTelegram/Imp/Telegram.imp.ts
@@ -17,19 +17,25 @@ class TelegramImp implements TelegramInterface.IAdapter {
 		this.Infrastructure = Infrastructure;
 	}
 
-	public sendMessage(text: string, chat_id: number, options?: TelegramInterface.IMessageOptions) {
+	public sendMessage(_text: string, chat_id: number, options?: TelegramInterface.IMessageOptions) {
+		const text = escapeTags(_text);
+
 		const btn = options?.buttons?.map((row) => row.map((cell) => ({ text: cell.text, callback_data: cell.click, url: cell.url })));
 		const data = { chat_id, text, parse_mode: options?.parseMode, reply_markup: { inline_keyboard: btn } };
 
 		return this.request<TelegramInterface.ISend>({ method: POST, link: ELink.SEND_MESSAGE, data });
 	}
 
-	public async sendManyMessage(text: string, chat_id: number, options?: TelegramInterface.IMessageOptions) {
+	public async sendManyMessage(_text: string, chat_id: number, options?: TelegramInterface.IMessageOptions) {
+		const text = escapeTags(_text);
+
 		const chunks = text.match(/[\s\S]{1,4000}(?=\s|$)/g) || [];
 		for (const chunk of chunks) await this.sendMessage(chunk, chat_id, options);
 	}
 
-	public editMessage(text: string, chat_id: number, message_id: number, options?: TelegramInterface.IMessageOptions) {
+	public editMessage(_text: string, chat_id: number, message_id: number, options?: TelegramInterface.IMessageOptions) {
+		const text = escapeTags(_text);
+
 		const btn = options?.buttons?.map((row) => row.map((cell) => ({ text: cell.text, callback_data: cell.click, url: cell.url })));
 		const data = { chat_id, text, message_id, parse_mode: options?.parseMode, reply_markup: { inline_keyboard: btn } };
 
@@ -56,6 +62,17 @@ class TelegramImp implements TelegramInterface.IAdapter {
 	public setCommand(commands: TelegramInterface.ICommand[], forUser?: number) {
 		return this.request({ method: POST, link: ELink.SET_DIRECTIVES, data: { commands, scope: forUser && { type: "chat", chat_id: forUser } } });
 	}
+}
+
+function escapeTags(input: string): string {
+	const allowedTags = ["b", "i", "u", "s", "code", "pre", "blockquote"];
+
+	return input.replace(/<\/?([a-zA-Z0-9]+)([^>]*)>/g, (match, tagName) => {
+		const lowerTag = tagName.toLowerCase();
+		if (allowedTags.includes(lowerTag)) return match;
+
+		return match.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	});
 }
 
 export default TelegramImp;
